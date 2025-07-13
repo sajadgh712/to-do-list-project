fa183496c8c80927e2124af5441ad85531ce6152
sajadgh712 <sajadgharedaghi.web@gmail.com>
2025-07-13 21:06:38 +0330

    chage style

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let modal = document.querySelector(".modal");
 let closeModal = document.getElementById("closeModal");
 let noBtn  =document.getElementById("noBtn");
 let yesBtn  =document.getElementById("yesBtn");
-
+let selectedTodo = null;
 
 function addNewTodo(newToDoValue) {
   let newTodoLi = document.createElement("li");
@@ -15,39 +15,41 @@ function addNewTodo(newToDoValue) {
   let newTodoTitleSpan = document.createElement("span");
   newTodoTitleSpan.innerHTML = newToDoValue;
 
-  let newTodoTrash = document.createElement("i");
-  newTodoTrash.className = "delete";
+  let newTodoTrash = document.createElement("button");
+  newTodoTrash.className = "delete btn btn-outline-danger btn-sm";
   newTodoTrash.innerHTML = "ClickForDelete"
 
   closeModal.addEventListener("click",function(){
     modal.style.display = "none"
   });
 
-  newTodoTrash.addEventListener("click", function (event) {
+  newTodoTrash.addEventListener("click", function () {
     if (modal.style.display = "none") {
       modal.style.display = "block"
+      selectedTodo = this.parentElement;
     }
-    
-   
-
   });
   
-  // yesBtn.addEventListener("click",function(event){
-  //   event.target.parentElement.remove();
-  // })
-  
-
+  yesBtn.addEventListener("click",function(){
+    if(selectedTodo) selectedTodo.remove();
+    
+    modal.style.display = "none";
+    selectedTodo = "null"
+  })
 
   noBtn.addEventListener("click",function(){
     modal.style.display = "none"
   })
-  // console.log(newTodoTrash);
 
   newTodoLi.append(newTodoTitleSpan, newTodoTrash);
 
   todoUlElem.append(newTodoLi);
 
   console.log(newTodoLi);
+
+  yesBtn.addEventListener("click", () =>{
+    modal.style.toggle("closeModal")
+  })
 }
 
 addToDoForm.addEventListener("submit", function (event) {
